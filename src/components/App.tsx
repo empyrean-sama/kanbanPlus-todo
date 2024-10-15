acb6680214a6438a47836a9cca3833a8234ddbe0
Sriveer Neerukonda <Sriveer.NEERUKONDA@outlook.com>
2024-10-15 06:57:05 +0530

    feat: Add new dependency, react-icons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import { FaAngleDown } from "react-icons/fa";
 
 export default function App() {
-    return <h1 className='title is-1'>Hello world from react app</h1>
+    return <h1 className='title is-1'>Hello world from react app.. <FaAngleDown /></h1>
 }
