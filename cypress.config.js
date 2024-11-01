e0f5236c1c9b2c4402c199f95069f37e7628eae6
Sriveer Neerukonda <Sriveer.NEERUKONDA@outlook.com>
2024-11-01 06:44:05 +0530

    feat: add new test for testing add board
    Also added a baseUrl configuration for cypress.config.js that is correct if manually setup (CI and CD not taken into account)

--- cypress.config.js
+++ cypress.config.js
@@ -4,8 +4,7 @@ module.exports = defineConfig({
   e2e: {
     viewportWidth: 1920,
     viewportHeight: 1080,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:1234',
+    setupNodeEvents(on, config) {},
   },
 });
