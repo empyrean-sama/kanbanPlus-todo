79d2d4ab41d07cf4984d636ed751177e596b757e
Sriveer Neerukonda <Sriveer.NEERUKONDA@outlook.com>
2024-12-27 05:49:59 +0530

    feat: handling type and story point change in fields of the edit page

--- src/components/Board/EditPage/Fields.tsx
+++ src/components/Board/EditPage/Fields.tsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import Style from "./Fields.module.scss";
 import { DropdownItem } from "../../ui/Dropdown";
+import capitalize from "just-capitalize";
 
 import LabeledGrid from "../../ui/LabeledInputs/LabeledGrid";
 import LabeledTextInput from "../../ui/LabeledInputs/LabeledTextInput";
 import LabeledNumberInput from "../../ui/LabeledInputs/LabeledNumberInput";
 import LabeledDropdown from "../../ui/LabeledInputs/LabeledDropdown";
 import LabeledDateTimePicker from "../../ui/LabeledInputs/LabeledDateTimePicker";
+import { editPageContext, IEditPageContext } from "./EditPage";
+
+import { ECardType } from "../../../Enum/ECardType";
 
 export default function Fields() {
+
+    const editPageAPI = useContext(editPageContext) as IEditPageContext ;
+    const editCard = editPageAPI.getEditCard();
+
+    function handleStoryPointsInput(ev: React.FormEvent<HTMLInputElement>) {
+        editPageAPI.setEditCard((editCard) => {
+            editCard.storyPoints = Number.parseInt((ev.target as HTMLInputElement).value)
+        });
+    }
+
+    function handleTypeChange(newSelectedId: string) {
+        editPageAPI.setEditCard((editCard) => {
+            editCard.type = newSelectedId.toLowerCase() as ECardType;
+        })
+    }
 
     return(
         <section className={Style['field-section']}>
             <LabeledGrid>
-                <LabeledTextInput id="uuid" disabled />
-                <LabeledDropdown id="type" placeholder="undefined" initialActiveId="task" onSelect = {() => {}}>
-                    <DropdownItem id="task">task</DropdownItem>
-                    <DropdownItem id="regression">regression</DropdownItem>
+                <LabeledTextInput id="uuid" disabled value={editCard.uuid} />
+                <LabeledDropdown  id="board" placeholder="undefined">
+                    
                 </LabeledDropdown>
-                <LabeledNumberInput id="story points" />
+                <LabeledDropdown  id="type" placeholder="undefined" initialActiveId={capitalize(editCard.type)} onSelect={handleTypeChange}>
+                    <DropdownItem id="Task">Task</DropdownItem>
+                    <DropdownItem id="Regression">Regression</DropdownItem>
+                </LabeledDropdown>
+                <LabeledNumberInput id="story points" value={editCard.storyPoints.toString()} onInput={handleStoryPointsInput} />
             </LabeledGrid>
             <LabeledGrid>
                 <LabeledDateTimePicker id="filed date" selectTime disabled/>
                 <LabeledDateTimePicker id="due date" selectTime/>
-                <LabeledNumberInput id="Time Spent" disabled />
+                <LabeledNumberInput id="estimated time" />
+                <LabeledNumberInput id="time spent" disabled />
             </LabeledGrid>        
         </section>
     )
